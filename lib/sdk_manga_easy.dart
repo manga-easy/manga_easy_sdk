54da45f75cd98597213628901434d9eac8efad6c
lucas <lucas.cardo123@hotmail.com>
2022-04-14 02:07:09 -0400

    add exports

--- lib/sdk_manga_easy.dart
+++ lib/sdk_manga_easy.dart
@@ -5,5 +5,18 @@ library sdk_manga_easy;
 
 export 'src/sdk_manga_easy_base.dart';
 export 'src/helpes.dart';
+export 'src/models/banner_model.dart';
+export 'src/models/recomendacoes_model.dart';
+export 'src/models/nivel_user.dart';
+export 'src/models/manga.dart';
+export 'src/models/biblioteca.dart';
+export 'src/models/chapter.dart';
+export 'src/models/detalhesManga.dart';
+export 'src/models/emblema.dart';
+export 'src/models/emblema_user.dart';
+export 'src/models/gender.dart';
+export 'src/models/generos_model.dart';
+export 'src/models/historico.dart';
+export 'src/models/imageChapters.dart';
 
 // TODO: Export any libraries intended for clients of this package.
