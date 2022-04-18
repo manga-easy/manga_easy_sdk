97788c5602c12b1fc9ff46ba320ac522491642c1
lucas <lucas.cardo123@hotmail.com>
2022-04-17 20:27:26 -0400

    add model notificacaoCapitulos

---
+++ lib/src/models/notificacao_capitulos.dart
@@ -0,0 +1,28 @@
+class NotificacaoCapitulos {
+  static String get collectionId => '6240955e6c241a37e6b2';
+  String? id;
+  String uniqueid;
+  String message;
+  String idHost;
+  List<String> capitulos;
+  String nomeManga;
+  NotificacaoCapitulos({
+    this.id,
+    required this.message,
+    required this.uniqueid,
+    required this.idHost,
+    required this.capitulos,
+    required this.nomeManga,
+  });
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['\$id'] = id;
+    data['uniqueid'] = uniqueid;
+    data['idHost'] = idHost;
+    data['menssege'] = message;
+    data['nomeManga'] = nomeManga;
+    data['capitulos'] = capitulos;
+    return data;
+  }
+}
