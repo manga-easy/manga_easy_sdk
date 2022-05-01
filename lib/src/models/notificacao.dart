2aa1197bb9dbb8bf8ee7e98e79c172f51dddb082
lucas <lucas.cardo123@hotmail.com>
2022-04-30 21:25:43 -0400

    remove payload das notificações

--- lib/src/models/notificacao.dart
+++ lib/src/models/notificacao.dart
@@ -3,24 +3,20 @@ class Notificacao {
   String? id;
   String titulo;
   String menssege;
-  String? payload;
   Notificacao({
     this.id,
     required this.menssege,
     required this.titulo,
-    this.payload,
   });
   Notificacao.fromJson(Map<String, dynamic> json)
       : titulo = json['titulo'],
         menssege = json['menssege'],
-        payload = json['payload'],
         id = json['\$id'];
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = {};
     data['\$id'] = id;
     data['titulo'] = titulo;
-    data['payload'] = payload;
     data['menssege'] = menssege;
     return data;
   }
