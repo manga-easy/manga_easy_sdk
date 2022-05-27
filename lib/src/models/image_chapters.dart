7af6f005ffad2578d52483ababdfcbe83433fb5f
lucas <lucas.cardo123@hotmail.com>
2022-05-26 20:38:31 -0400

    add campo state na model image_chapter

--- lib/src/models/image_chapters.dart
+++ lib/src/models/image_chapters.dart
@@ -1,16 +1,15 @@
-import 'dart:io';
-
 class ImageChapter {
   String? id;
   String src;
   String? path;
-  File? file;
+  int state;
 
-  ImageChapter({required this.src});
+  ImageChapter({required this.src, required this.state});
 
   ImageChapter.fromJson(dynamic json)
       : id = json['id'],
         path = json['path'],
+        state = json['state'] ?? 1,
         src = json['src'];
 
   Map<String, dynamic> toJson() {
@@ -18,6 +17,7 @@ class ImageChapter {
     data['id'] = id;
     data['src'] = src;
     data['path'] = path;
+    data['state'] = state;
     return data;
   }
 }
