95339c3fa06141eb43e024e5756af3f197844458
lucas cardoso <lucas.cardo123@hotmail.com>
2022-06-02 09:11:10 -0400

    add model InfoComic

---
+++ lib/src/models/info_comic_model.dart
@@ -0,0 +1,31 @@
+class InfoComicModel {
+  static String get collectionId => '62408a74a3081ee2886a';
+
+  final String name;
+  final String uniqueid;
+  final String ultimoCap;
+  final int totalViews;
+  final String autor;
+  final String thumb;
+  final String sinopse;
+  final double ratio;
+  final String scans;
+  final int dateUp;
+  final int dateMade;
+  final String generos;
+
+  InfoComicModel({
+    required this.name,
+    required this.uniqueid,
+    required this.ultimoCap,
+    required this.totalViews,
+    required this.autor,
+    required this.thumb,
+    required this.sinopse,
+    required this.ratio,
+    required this.scans,
+    required this.dateUp,
+    required this.dateMade,
+    required this.generos,
+  });
+}
