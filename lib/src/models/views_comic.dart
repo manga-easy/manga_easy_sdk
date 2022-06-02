3a8315ee1913bf3882b566f490d348c6c6ec25ba
lucas cardoso <lucas.cardo123@hotmail.com>
2022-06-02 09:11:24 -0400

    add model ViewsComic

---
+++ lib/src/models/views_comic.dart
@@ -0,0 +1,13 @@
+class ViewsComics {
+  static String get collectionId => '6298b6f30ae9b0bf95b5';
+
+  final String uniqueid;
+  final String idUser;
+  final int dateCria;
+
+  ViewsComics({
+    required this.uniqueid,
+    required this.idUser,
+    required this.dateCria,
+  });
+}
