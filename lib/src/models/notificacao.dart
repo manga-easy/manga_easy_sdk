7b84f1a67a539298756fb072ca68d1e82fdb81d0
lucas <lucas.cardo123@hotmail.com>
2022-07-10 00:37:00 -0400

    add campo image nas notificação

--- lib/src/models/notificacao.dart
+++ lib/src/models/notificacao.dart
@@ -3,16 +3,19 @@ class Notificacao {
   String? id;
   String titulo;
   String menssege;
+  String image;
   int dateMade;
   Notificacao({
     this.id,
     required this.menssege,
     required this.titulo,
     required this.dateMade,
+    required this.image,
   });
   Notificacao.fromJson(Map<String, dynamic> json)
       : titulo = json['titulo'] ?? '',
         menssege = json['menssege'] ?? '',
+        image = json['image'] ?? '',
         dateMade = json['dateMade'] ?? DateTime.now().millisecondsSinceEpoch,
         id = json['\$id'];
 
@@ -20,6 +23,7 @@ class Notificacao {
     final Map<String, dynamic> data = {};
     data['\$id'] = id;
     data['titulo'] = titulo;
+    data['image'] = image;
     data['menssege'] = menssege;
     data['dateMade'] = dateMade;
     return data;
