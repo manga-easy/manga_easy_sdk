ede8117efa69c643d2b2d696746c3c9e4f4eb839
lucas <lucas.cardo123@hotmail.com>
2022-09-29 13:13:11 -0400

    Add model Host

---
+++ lib/src/modules/config/host.dart
@@ -0,0 +1,41 @@
+class HostModel {
+  static String get collectionId => '6228a4a974bc61d675d7';
+  String? id;
+  int idHost;
+  int order;
+  String name;
+  String host;
+  String status;
+  String interstitialAdUnitId;
+
+  HostModel({
+    this.id,
+    required this.name,
+    required this.host,
+    required this.order,
+    required this.status,
+    required this.interstitialAdUnitId,
+    required this.idHost,
+  });
+
+  HostModel.fromJson(Map<String, dynamic> json)
+      : id = json['\$id'],
+        name = json['name'],
+        order = json['order'],
+        status = json['status'],
+        idHost = json['idHost'],
+        interstitialAdUnitId = json['interstitialAdUnitId'],
+        host = json['host'];
+
+  Map<String, dynamic> toJson() {
+    return {
+      'status': status,
+      '\$id': id,
+      'name': name,
+      'host': host,
+      'order': order,
+      'idHost': idHost,
+      'interstitialAdUnitId': interstitialAdUnitId,
+    };
+  }
+}
