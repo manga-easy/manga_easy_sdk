9161097d6272fb7b37404924db0a37665ad3e0e5
lucas <lucas.cardo123@hotmail.com>
2022-11-18 19:41:02 -0400

    Add idHost in Manga model

--- lib/src/modules/manga/manga.dart
+++ lib/src/modules/manga/manga.dart
@@ -3,17 +3,20 @@ class Manga {
   String title;
   String href;
   String capa;
+  int idHost;
 
   Manga.fromValue({
     required this.capa,
     required this.href,
     required this.title,
+    required this.idHost,
   });
 
   Manga.fromJson(dynamic json)
       : id = json['\$id'],
         title = json['title'],
         href = json['href'],
+        idHost = json['idHost'],
         capa = json['capa'];
 
   Map<String, dynamic> toJson() {
@@ -22,6 +25,7 @@ class Manga {
     data['title'] = title;
     data['href'] = href;
     data['capa'] = capa;
+    data['idHost'] = idHost;
     return data;
   }
 }
