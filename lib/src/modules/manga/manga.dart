de61939a144e6967b3417fc276fe03e33243b9f1
lucas <lucas.cardo123@hotmail.com>
2022-11-19 01:05:19 -0400

    Add uniqueid in manga

--- lib/src/modules/manga/manga.dart
+++ lib/src/modules/manga/manga.dart
@@ -1,6 +1,10 @@
+import 'package:host_mangas/host_mangas.dart';
+import 'package:sdk_manga_easy/sdk_manga_easy.dart';
+
 class Manga {
   String? id;
   String title;
+  String uniqueid;
   String href;
   String capa;
   int idHost;
@@ -10,18 +14,24 @@ class Manga {
     required this.href,
     required this.title,
     required this.idHost,
+    required this.uniqueid,
   });
 
   Manga.fromJson(dynamic json)
       : id = json['\$id'],
         title = json['title'],
+        uniqueid = json['uniqueid'] ?? Helps.convertUniqueid(json['title']),
         href = json['href'],
-        idHost = json['idHost'],
+        idHost = json['idHost'] ??
+            IHostManga.retornaIdHost(
+              v: json['title'],
+            ),
         capa = json['capa'];
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
     data['\$id'] = id;
+    data['uniqueid'] = uniqueid;
     data['title'] = title;
     data['href'] = href;
     data['capa'] = capa;
