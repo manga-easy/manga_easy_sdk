db3a7db361f66b1a6eca3e97d34fea50eb3af4c2
lucas <lucas.cardo123@hotmail.com>
2023-02-22 18:51:23 -0400

    Update helpes.dart

--- lib/src/helpes.dart
+++ lib/src/helpes.dart
@@ -46,7 +46,7 @@ class Helps {
 
   static decode(value) {
     if (value is String) {
-      return json.decode(value);
+      return json.decode(value.replaceAll('\n', ''));
     }
     return value;
   }
