3cf6c233f34f900a127b49b636a5b342b60d820c
lucas <lucas.cardo123@hotmail.com>
2023-03-31 23:11:02 -0400

    Delete config model

--- lib/src/modules/core/global.dart
+++ lib/src/modules/core/global.dart
@@ -1,12 +1,10 @@
 import 'package:manga_easy_sdk/src/modules/config/block_list_model.dart';
-import 'package:manga_easy_sdk/src/modules/config/config_model.dart';
 import 'package:manga_easy_sdk/src/modules/user/user.dart';
 
 class Global {
   static bool isIntruce = true;
   static User? user;
   static Map<String, String> header = {};
-  static ConfigAppModel confApp = ConfigAppModel.offline();
   static bool filtraImg = true;
   static bool isInicializedApp = false;
   static bool isNotUnilinkUsed = true;
