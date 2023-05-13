945ca4162d336a6f6230820eb457f8c81e870b5d
lucas <lucas.cardo123@hotmail.com>
2023-05-12 21:39:53 -0400

    Update recomendacoes_model.dart

--- lib/src/modules/general/recomendacoes_model.dart
+++ lib/src/modules/general/recomendacoes_model.dart
@@ -1,10 +1,12 @@
 class RecomendacoesModel {
-  static const String collectionID = '6240a8375d93f7a0194c';
   String? id;
   String uniqueid;
   String title;
   String link;
+  @Deprecated('User createAt')
   int dataCria;
+  int createdAt;
+  int updatedAt;
 
   RecomendacoesModel({
     this.id,
@@ -12,22 +14,7 @@ class RecomendacoesModel {
     required this.link,
     required this.uniqueid,
     required this.dataCria,
+    required this.createdAt,
+    required this.updatedAt,
   });
-
-  RecomendacoesModel.fromJson(Map<String, dynamic> json)
-      : id = json['\$id'],
-        title = json['title'],
-        uniqueid = json['uniqueid'],
-        dataCria = json['dataCria'] ?? DateTime.now().millisecondsSinceEpoch,
-        link = json['link'];
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = {};
-    data['\$id'] = id;
-    data['title'] = title;
-    data['uniqueid'] = uniqueid;
-    data['link'] = link;
-    data['dataCria'] = dataCria;
-    return data;
-  }
 }
