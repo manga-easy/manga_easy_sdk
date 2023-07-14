9b0ebb55855fba5ae99f73bcd23a9feb866cf949
lucas <lucas.cardo123@hotmail.com>
2023-07-13 21:34:51 -0400

    Update recomendacoes_model.dart

--- lib/src/modules/general/recomendacoes_model.dart
+++ lib/src/modules/general/recomendacoes_model.dart
@@ -1,12 +1,14 @@
 class RecomendacoesModel {
-  String? id;
-  String uniqueid;
-  String title;
-  String link;
+  final String? id;
+  final String uniqueid;
+  final String title;
+  final String link;
   @Deprecated('User createAt')
-  int dataCria;
-  int createdAt;
-  int updatedAt;
+  final int dataCria;
+  final int createdAt;
+  final int updatedAt;
+  final String? artistId;
+  final String? artistName;
 
   RecomendacoesModel({
     this.id,
@@ -16,6 +18,8 @@ class RecomendacoesModel {
     required this.dataCria,
     required this.createdAt,
     required this.updatedAt,
+    this.artistId,
+    this.artistName,
   });
 
   factory RecomendacoesModel.empty() {
@@ -26,6 +30,30 @@ class RecomendacoesModel {
       dataCria: DateTime.now().millisecondsSinceEpoch,
       createdAt: DateTime.now().millisecondsSinceEpoch,
       updatedAt: DateTime.now().millisecondsSinceEpoch,
+    );
+  }
+
+  RecomendacoesModel copyWith({
+    String? id,
+    String? uniqueid,
+    String? title,
+    String? link,
+    int? dataCria,
+    int? createdAt,
+    int? updatedAt,
+    String? artistId,
+    String? artistName,
+  }) {
+    return RecomendacoesModel(
+      id: id ?? this.id,
+      uniqueid: uniqueid ?? this.uniqueid,
+      title: title ?? this.title,
+      link: link ?? this.link,
+      dataCria: dataCria ?? this.dataCria,
+      createdAt: createdAt ?? this.createdAt,
+      updatedAt: updatedAt ?? this.updatedAt,
+      artistId: artistId ?? this.artistId,
+      artistName: artistName ?? this.artistName,
     );
   }
 }
